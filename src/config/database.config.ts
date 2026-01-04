f005b43751b7a631340ac76d1bcb919cbda26c6e
Junior500142 <eliassjuniior1423@gmail.com>
2026-01-04 12:00:35 -0400

    Mudança no banco de dados para Aiven

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,19 +3,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '4000'),
+  port: parseInt(process.env.DB_PORT || '25955'),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true, 
+  synchronize: true,
+  logging: true,
   ssl: {
     rejectUnauthorized: false
-  },
-  connectorPackage: 'mysql2',
-  extra: {
-    ssl: {
-      rejectUnauthorized: false
-    }
   }
 };
