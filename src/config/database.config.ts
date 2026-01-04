b6d87e9c9ee62422a47b31ecc321784456ae9a7e
Junior500142 <eliassjuniior1423@gmail.com>
2026-01-03 23:43:24 -0400

    alteração na url do banco de dados para deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,23 +2,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '4000'), 
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'test',
+  url: process.env.DATABASE_URL, 
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: false, 
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
   migrationsRun: true,
   logging: process.env.NODE_ENV === 'development',
   ssl: {
-    minVersion: 'TLSv1.2',
-    rejectUnauthorized: true 
-  },
-  extra: {
-    ssl: {
-      rejectUnauthorized: true
-    }
+    rejectUnauthorized: true
   }
 };
