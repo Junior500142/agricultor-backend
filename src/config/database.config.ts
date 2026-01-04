afda061acf4b5caccdcca8272a792fba17e62c7e
Junior500142 <eliassjuniior1423@gmail.com>
2026-01-03 23:22:10 -0400

    Correção nas configurações do DB para deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,16 +3,22 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '3306'),
+  port: parseInt(process.env.DB_PORT || '4000'), 
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'agricultores_db',
+  database: process.env.DB_DATABASE || 'test',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: false, 
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
   migrationsRun: true,
   logging: process.env.NODE_ENV === 'development',
   ssl: {
-    rejectUnauthorized: false, 
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true 
   },
+  extra: {
+    ssl: {
+      rejectUnauthorized: true
+    }
+  }
 };
