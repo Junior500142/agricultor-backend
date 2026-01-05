738ad7e747eb39483c43d7cfc6496747fd863bab
Junior500142 <eliassjuniior1423@gmail.com>
2026-01-05 13:42:24 -0400

    SSL dinâmico

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AgricultoresModule } from './agricultores/agricultores.module';
-import { databaseConfig } from './config/database.config';
+import { Agricultor } from './agricultores/entities/agricultor.entity';
 
 @Module({
   imports: [
@@ -12,7 +12,27 @@ import { databaseConfig } from './config/database.config';
       isGlobal: true,
     }),
     
-    TypeOrmModule.forRoot(databaseConfig),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '25955', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      
+      entities: [Agricultor], 
+      synchronize: true, 
+      logging: true,
+    
+      ssl: process.env.DB_PORT === '3306' ? false : { rejectUnauthorized: false },
+      extra: {
+        connectionLimit: 10,
+        waitForConnections: true,
+        queueLimit: 0,
+        keepAliveInitialDelay: 10000,
+        enableKeepAlive: true,
+      },
+    }),
     
     AgricultoresModule,
   ],
